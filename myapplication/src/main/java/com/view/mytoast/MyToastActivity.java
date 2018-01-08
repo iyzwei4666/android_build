ef7877b478c54d0bda3d1b27e83c85ddb9c45627
weiyongzhi <123456>
2018-01-08 19:41:47 +0800

    TranslateAnimation

--- myapplication/src/main/java/com/view/mytoast/MyToastActivity.java
+++ myapplication/src/main/java/com/view/mytoast/MyToastActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
+
 import com.example.myapplication.R;
 import com.mytoast.TastyToast;
 
@@ -35,8 +39,26 @@ public class MyToastActivity extends AppCompatActivity {
     }
 
     public void showInfoToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "Searching for username : 'Rahul' ", TastyToast.LENGTH_LONG,
-                TastyToast.INFO);
+//        TastyToast.makeText(getApplicationContext(), "Searching for username : 'Rahul' ", TastyToast.LENGTH_LONG,
+//                TastyToast.INFO);
+        LinearLayout container3 = (LinearLayout) findViewById(R.id.linearLayout);
+        int childCount = container3.getChildCount() ;
+        for (int i = 0; i < childCount/2; i++) {
+            View uv = container3.getChildAt(i);
+            View dv = container3.getChildAt(childCount - 1 - i);
+            int y1 = uv.getTop();
+            int y2 = dv.getTop();
+
+            Animation translateAnimation = new TranslateAnimation(0, 0 , 0 , y2);
+            translateAnimation.setDuration(300);
+            translateAnimation.setFillAfter(true);//设置动画结束后保持当前的位置（即不返回到动画开始前的位置）
+            uv.startAnimation(translateAnimation);
+
+            Animation translateAnimation2 = new TranslateAnimation(0, 0 , y1 , 0 );
+            translateAnimation2.setDuration(300);
+            translateAnimation2.setFillAfter(true);//设置动画结束后保持当前的位置（即不返回到动画开始前的位置）
+            dv.startAnimation(translateAnimation2);
+        }
     }
 
     public void showDefaultToast(View view) {
