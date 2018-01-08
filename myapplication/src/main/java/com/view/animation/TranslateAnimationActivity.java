508dff52690591676b893fcc87073e8ca66250f8
weiyongzhi <123456>
2018-01-08 22:59:41 +0800

    anim

--- myapplication/src/main/java/com/view/animation/TranslateAnimationActivity.java
+++ myapplication/src/main/java/com/view/animation/TranslateAnimationActivity.java
@@ -28,45 +28,47 @@ public class TranslateAnimationActivity extends AppCompatActivity {
         container3 = (LinearLayout) findViewById(R.id.linearLayout);
         list = new ArrayList<>();
     }
-    LinearLayout container3 ;
-    List<View> list ;
+
+    LinearLayout container3;
+    List<View> list;
+
     public void showAnim(View view) {
         list.clear();
-        int childCount = container3.getChildCount() ;
-        for (int i = 0 ; i < childCount  ; i++) {
+        int childCount = container3.getChildCount();
+        for (int i = 0; i < childCount; i++) {
             View uv = container3.getChildAt(i);
             list.add(uv);
         }
 
-        for (int i = 0 ; i < childCount/2 ; i++) {
+        for (int i = 0; i < childCount / 2; i++) {
             View uv = container3.getChildAt(i);
             View dv = container3.getChildAt(childCount - 1 - i);
             int y1 = uv.getTop();
             int y2 = dv.getTop();
 
-            Animation translateAnimation = new TranslateAnimation(0, 0 , y1 , y2 - y1);
+            Animation translateAnimation = new TranslateAnimation(0, 0, y1, y2 - y1);
             translateAnimation.setDuration(300);
             translateAnimation.setFillAfter(true);//设置动画结束后保持当前的位置（即不返回到动画开始前的位置）
             uv.startAnimation(translateAnimation);
 
-            Animation translateAnimation2 = new TranslateAnimation(0,0  ,-y1 ,y1 -y2 );
+            Animation translateAnimation2 = new TranslateAnimation(0, 0, -y1, y1 - y2);
             translateAnimation2.setDuration(300);
             translateAnimation2.setFillAfter(true);//设置动画结束后保持当前的位置（即不返回到动画开始前的位置）
             dv.startAnimation(translateAnimation2);
         }
-        handle.sendEmptyMessageDelayed(0 , 300);
+        handle.sendEmptyMessageDelayed(0, 300);
 
     }
- Handler handle = new Handler() {
-     @Override
-     public void handleMessage(Message msg) {
 
-         container3.removeAllViews();
-         Collections.reverse(list);
-         for (View v:list
-                 ) {
-             container3.addView(v);
-         }
-     }
- };
+    Handler handle = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            container3.removeAllViews();
+            Collections.reverse(list);
+            for (View v : list  ) {
+                container3.addView(v);
+            }
+        }
+    };
 }
