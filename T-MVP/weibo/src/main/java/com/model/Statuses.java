7ae6465be22ec8e12f9f7ae2e61df799c9488965
weiyongzhi <123456>
2018-04-25 03:09:26 +0800

    mvp

---
+++ T-MVP/weibo/src/main/java/com/model/Statuses.java
@@ -0,0 +1,8 @@
+package com.model;
+
+/**
+ * Created by weiyongzhi on 2018/4/24.
+ */
+
+public class Statuses {
+}
---
+++ T-MVP/weibo/src/main/java/com/ui/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ken.weibo.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
