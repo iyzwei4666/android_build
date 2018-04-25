96c1eff36992e5b67e90a2fd0f8d64389aaa55c3
weiyongzhi <123456>
2018-04-25 09:27:48 +0800

    del

--- T-MVP/app/src/main/java/com/base/adapter/TypeSelector.java
+++
@@ -1,9 +0,0 @@
-package com.base.adapter;
-
-/**
- * Created by baixiaokang on 16/12/30.
- */
-
-public interface TypeSelector<M> {
-    int getType(M m);
-}
--- T-MVP/weibo/src/main/java/com/model/Statuses.java
+++
@@ -1,8 +0,0 @@
-package com.model;
-
-/**
- * Created by weiyongzhi on 2018/4/24.
- */
-
-public class Statuses {
-}
