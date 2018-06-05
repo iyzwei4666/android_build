d7de25f8a958ba94073e06f54aecac9112badaa4
weiyongzhi <123456>
2018-06-05 11:17:41 +0800

    bottommenu

--- bottomsheet/src/main/java/com/example/myapplication/MainActivity.java
+++ bottomsheet/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,123 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.myapplication.behavior.AnchorBottomSheetBehavior;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.topLayout)
+    RelativeLayout topLayout;
+    @BindView(R.id.bottom_sheet)
+    NestedScrollView bottomSheet;
+    @BindView(R.id.bottom_sheet_menu)
+    LinearLayout bottomSheetMenu;
+    @BindView(R.id.bottom_sheet_menu_detail_map)
+    TextView bottomSheetMenuDetailMap;
+    @BindView(R.id.bottom_sheet_menu_navi)
+    TextView bottomSheetMenuNavi;
+    @BindView(R.id.bottom_sheet_menu_internal)
+    TextView bottomSheetMenuInternal;
+    @BindView(R.id.bottom_sheet_menu_production_ledger)
+    TextView bottomSheetMenuProductionLedger;
+    @BindView(R.id.top_bar)
+    RelativeLayout topBar;
+    private AnchorBottomSheetBehavior mBehavior;
+    private int mHeight = 150;
+    private float mOffset = 0.0f;
+    private int mSheetHeight = 120;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        context = this;
+        topLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bottomSheetMenu.getVisibility() == View.VISIBLE) {
+                    bottomSheet.setVisibility(View.GONE);
+                    bottomSheetMenu.setVisibility(View.GONE);
+                } else {
+                    bottomSheet.setVisibility(View.VISIBLE);
+                    bottomSheetMenu.setVisibility(View.VISIBLE);
+
+                }
+                mBehavior.setState(AnchorBottomSheetBehavior.STATE_COLLAPSED);
+
+            }
+        });
+        bottomSheetMenuDetailMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBehavior.getState() == AnchorBottomSheetBehavior.STATE_COLLAPSED) {
+                    mBehavior.setState(AnchorBottomSheetBehavior.STATE_ANCHOR_POINT);
+                } else {
+                    mBehavior.setState(AnchorBottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+        });
+        mBehavior = AnchorBottomSheetBehavior.from(bottomSheet);
+        int height = ScreenUtil.getScreenHeight(MainActivity.this) / 3;
+        mBehavior.setAnchorPoint(height);
+        mBehavior.addBottomSheetCallback(new AnchorBottomSheetBehavior.BottomSheetCallback() {
+            private int oldState = 0;
+
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+                switch (newState) {
+                    case AnchorBottomSheetBehavior.STATE_COLLAPSED:
+                        Log.d("bottomsheet-", "STATE_COLLAPSED");
+                        bottomSheetMenuDetailMap.setText("详情");
+                        oldState = newState;
+                        break;
+                    case AnchorBottomSheetBehavior.STATE_DRAGGING:
+                        Log.d("bottomsheet-", "STATE_DRAGGING");
+                        if (oldState == AnchorBottomSheetBehavior.STATE_COLLAPSED)
+                            bottomSheetMenuDetailMap.setText("地图");
+                        break;
+                    case AnchorBottomSheetBehavior.STATE_EXPANDED:
+                        Log.d("bottomsheet-", "STATE_EXPANDED");
+                        oldState = newState;
+                        break;
+                    case AnchorBottomSheetBehavior.STATE_ANCHOR_POINT:
+                        Log.d("bottomsheet-", "STATE_ANCHOR_POINT");
+                        bottomSheetMenuDetailMap.setText("地图");
+                        oldState = newState;
+                        break;
+                    case AnchorBottomSheetBehavior.STATE_HIDDEN:
+                        Log.d("bottomsheet-", "STATE_HIDDEN");
+                        bottomSheetMenuDetailMap.setText("详情");
+                        break;
+                    default:
+                        Log.d("bottomsheet-", "STATE_SETTLING");
+
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                Log.d("bottomsheet-", "slideOffset:"+slideOffset);
+                mOffset = slideOffset;
+                topLayout.animate().translationY(-300 * (  slideOffset ));
+            }
+        });
+        mBehavior.setState(AnchorBottomSheetBehavior.STATE_COLLAPSED);
     }
 }
--- bottomsheet/src/main/java/com/example/myapplication/behavior/TopLayoutBehavior.java
+++ bottomsheet/src/main/java/com/example/myapplication/behavior/TopLayoutBehavior.java
@@ -1,14 +1,17 @@
 package com.example.myapplication.behavior;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 
 import com.example.myapplication.R;
+import com.example.myapplication.ScreenUtil;
 
 import java.lang.ref.WeakReference;
 
@@ -48,14 +51,17 @@ public class TopLayoutBehavior extends CoordinatorLayout.Behavior<View>{
         int height=view.getHeight();
         int bottom=view.getBottom();
         int top=dependency.getTop();
+        int offset = (int) (ScreenUtil.getScreenHeight((Activity) mTopLayoutRef.get().getContext()) * 0.5);
 
-        if (top-500<height){
-            ViewCompat.offsetTopAndBottom(view,(top-500-bottom));
+        if (top-offset<height ) {
+//            Log.e("TopLayoutBehavior1",""+(top-offset-bottom));
+//            ViewCompat.offsetTopAndBottom(view,(top-offset-bottom));
             return true;
         }
 
-        if (top>height+500 && bottom<height){
-            ViewCompat.offsetTopAndBottom(view,(height-bottom));
+        if (top>height+offset && bottom<height){
+//            Log.e("TopLayoutBehavior2",""+(height-bottom));
+//            ViewCompat.offsetTopAndBottom(view,(height-bottom));
         }
         return super.onDependentViewChanged(parent,child,dependency);
 
